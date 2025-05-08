a7e88509e31142de7dcdb8a8801de0fc8e66e32c
dianbayunugroho <bayu@newtronic-solution.com>
2025-05-08 14:59:04 +0700

    feat: ui delete data course

--- src/modules/master-data/course/views/CourseListView.tsx
+++ src/modules/master-data/course/views/CourseListView.tsx
@@ -6,27 +6,38 @@ import DashboardContent from '../../../../components/layout/main/DashboardConten
 import CustomTable from '../../../../components/table/CustomTable';
 import ActionTableButton from '../../../../components/button/ActionTableButton';
 import CourseTableFilter from '../components/CourseTableFilter';
+import ModalDelete from '../../../../components/modal/ModalDelete';
+const data: CourseEntity[] = [
+  {
+    id: '1',
+    name: 'Matematika',
+    createAt: '05 Mei 2025',
+  },
+  {
+    id: '2',
+    name: 'Bahasa Indonesia',
+    createAt: '05 Mei 2025',
+  },
+  {
+    id: '3',
+    name: 'Bahasa Inggris',
+    createAt: '05 Mei 2025',
+  },
+];
 
 export default function CourseListView() {
   const [isLoading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [course, setCourse] = useState<CourseEntity>();
+  const [listCourse, setListCourse] = useState<CourseEntity[]>(data);
+
+  const deleteCourseById = (id?: string) => {
+    setListCourse((prevData) => prevData.filter((value) => value.id !== id));
+  };
   const headers = [{ label: 'Nama Pelajaran' }, { label: 'Tanggal dibuat' }, { label: 'Aksi' }];
-  const data: CourseEntity[] = [
-    {
-      id: '1',
-      name: 'Matematika',
-      createAt: '05 Mei 2025',
-    },
-    {
-      id: '2',
-      name: 'Bahasa Indonesia',
-      createAt: '05 Mei 2025',
-    },
-    {
-      id: '3',
-      name: 'Bahasa Inggris',
-      createAt: '05 Mei 2025',
-    },
-  ];
+
   useEffect(() => {
     setTimeout(() => setLoading(false), 2000);
   }, [isLoading]);
@@ -37,21 +48,35 @@ export default function CourseListView() {
         action={<Button>Tambah</Button>}
       />
       <DashboardContent>
+        <ModalDelete
+          id={course?.id}
+          name={course?.name}
+          open={open}
+          onClose={handleClose}
+          onDelete={() => deleteCourseById(course?.id)}
+        />
         <CourseTableFilter />
         <CustomTable
           headers={headers}
           count={100}
           page={1}
           isLoading={isLoading}
-          isEmpty={data.length === 0}
-          data={data}
+          isEmpty={listCourse.length === 0}
+          data={listCourse}
           render={(course) => {
             return (
               <TableRow key={course.id}>
                 <TableCell>{course.name}</TableCell>
                 <TableCell>{course.createAt}</TableCell>
                 <TableCell>
-                  <ActionTableButton />
+                  <ActionTableButton
+                    onClickDetail={() => {}}
+                    onClickEdit={() => {}}
+                    onClickDelete={() => {
+                      setCourse(course);
+                      handleOpen();
+                    }}
+                  />
                 </TableCell>
               </TableRow>
             );
