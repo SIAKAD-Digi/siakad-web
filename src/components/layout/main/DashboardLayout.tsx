21a7db0f1f5cdd788952047c14bacb1db234a2a5
Mardini <mardini1414@gmail.com>
2025-06-10 20:43:20 +0700

    fix sidebar menu and path error

--- src/components/layout/main/DashboardLayout.tsx
+++ src/components/layout/main/DashboardLayout.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router';
+import { useState, useEffect } from 'react';
 import { Box, colors } from '@mui/material';
+import { Outlet, useLocation } from 'react-router';
 
 import Main from './Main';
 import Navbar from './Navbar';
@@ -8,6 +8,12 @@ import Sidebar from './Sidebar';
 
 export const DashboardLayout = () => {
   const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setOpen(false);
+  }, [location.pathname]);
+
   return (
     <Box bgcolor={colors.grey[100]} minHeight="100vh" pt={12}>
       <Navbar setOpen={setOpen} />
--- src/middleware/RoleMiddleware.tsx
+++ src/middleware/RoleMiddleware.tsx
@@ -12,11 +12,11 @@ export default function RoleMiddleware({ roles, element }: Props) {
   const role = localStorage.getItem('role');
 
   if (role === null) {
-    return <Navigate to={pathConfig.auth.forbiden} />;
+    return <Navigate to={pathConfig.error.forbiden} />;
   }
 
   if (roles && roles?.includes(role as Role)) {
-    return <Navigate to={pathConfig.auth.forbiden} />;
+    return <Navigate to={pathConfig.error.forbiden} />;
   }
 
   return element;
